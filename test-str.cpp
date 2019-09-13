9c647f65d430bd515f74341eb253828548bea739
Lerwox <lerwox@gmail.com>
2019-09-13 22:43:21 +0200

    Adding digramme frequency

--- test-str.cpp
+++ test-str.cpp
@@ -40,14 +40,15 @@ std::map<std::string, float>* freqLetter(/*out*/ unsigned long& nbIte, std::stri
     }
 
     /////////////////////////////////////
-    //Ceci fonctionne ^^
-    map->emplace(std::string(1, 'ç'), 0);
-    map->emplace(std::string(1, '🙃'), 0);
+    //Ceci fonctionne ^^ MAIS bon le prof en a pas besoin
+    //map->emplace(std::string(1, 'ç'), 0);
+    //map->emplace(std::string(1, '🙃'), 0);
     /////////////////////////////////////
 
     char c; //Caractère lut
     unsigned long count = 0;    //Compte le nombre de caractère pris en compte dans l'analyse
     std::map<std::string, int>::iterator it;    //créé un itérateur sur la map
+
     while (fic.get(c)) {    //Tant que il y a des caractères a lire
         it = map->find(std::string(1, c));  //L'itérateur prend la valeur de la case de la map qui contient le caractère s'il existe, sinon il prend la fin de la liste
         if(it != map->end()) {  //Si le caractère existe dans la map
@@ -57,12 +58,49 @@ std::map<std::string, float>* freqLetter(/*out*/ unsigned long& nbIte, std::stri
     }
     
     nbIte = count;  //Le nombre passé en paramètre prend la valeur du nombre de caractères analysés pris en compte
-    fic.close();    //ferme le fichier (libère la mémoire je suppose)
-
     std::map<std::string, float>* fmap = calcFrequence(map->begin(), map->end(), count);    //Créer les fréquence et la map finale
 
+    fic.close();    //ferme le fichier (libère la mémoire je suppose)
     delete map;     //delete la map temporaire
     return fmap;    //retourne la map avec les fréquence
+}
+
+
+std::map<std::string, float>* freqDigramme(/*out*/ unsigned long& nbIte, std::string path) {
+    std::ifstream fic;
+    fic.open(path, std::ios_base::in);  //Ouvre le fichier en lecture
+    if( !fic.is_open() )    //Si le fichier n'est pas ouvert
+        throw std::ios_base::failure("Impossible d'ouvrir le fichier: \"" + path + "\" !"); //Alors on émet une erreur
+
+    std::map<std::string, int> * map = new std::map<std::string, int>;  //Créer une map avec pour clé les caractère et pour valeur leur itération
+
+    //Génère les digrammes
+    for (char letter1 = 'a'; letter1 <= 'z'; ++letter1) { 
+        for (char letter2 = 'a'; letter2 <= 'z'; ++letter2) {
+            map->emplace(std::string() + letter1 + letter2, 0);    //rempli la map avec les digrammes initialisé à 0
+        }
+    }
+
+    char c; //Caractère lut
+    std::string digramme = "";  //Variable temporaire qui prend le caractère extrait + le suivant (non  extrait)
+    unsigned long count = 0;    //Compte le nombre de caractère pris en compte dans l'analyse
+    std::map<std::string, int>::iterator it;    //créé un itérateur sur la map
+
+    while (fic.get(c)) {    //Tant que il y a des caractères a lire
+        digramme = std::string() + c + (char) fic.peek();
+        it = map->find(digramme);  //L'itérateur prend la valeur de la case de la map qui contient le caractère s'il existe, sinon il prend la fin de la liste
+        if(it != map->end()) {  //Si le caractère existe dans la map
+            ++it->second;   //Incrémente la valeur du caractère
+            ++count;    //Incrémente le nombre de caractère pris en compte dans l'analyse
+        }
+    }
+
+    nbIte = count;  //Le nombre passé en paramètre prend la valeur du nombre de caractères analysés pris en compte
+    std::map<std::string, float>* fmap = calcFrequence(map->begin(), map->end(), count);    //Créer les fréquence et la map finale
+
+    fic.close();
+    delete map;
+    return fmap;
 }
 
 
@@ -70,20 +108,34 @@ int main() {
     std::string path = "./test.txt";    //Il faudrai le passé en argument du programme
     unsigned long nbIte = 0;  //Variable qui prend le nombre d'itération
     std::map<std::string, float> * freqLet;
+    std::map<std::string, float> * freqDig;
 
     try {
         freqLet = freqLetter(nbIte, path);   //Récupère la map avec les fréquences du fichier
-        std::cout << freqLet->size() << ", nbCar = " << nbIte << std::endl; //Affiche la taille de la map et le nombre de caractère analysé pris en compte
+        
+        //A ecrire dans un fichier
+        /*std::cout << freqLet->size() << ", nbCar = " << nbIte << std::endl; //Affiche la taille de la map et le nombre de caractère analysé pris en compte
 
         for (std::map<std::string, float>::iterator it = freqLet->begin(); it != freqLet->end(); ++it) {    //Parcours de la liste
             std::cout << it->first << " has value " << it->second << std::endl; //Pour chaque élément on affiche le caractère et sa fréquence
         }
+        */
+
+        freqDig = freqDigramme(nbIte, path);
+        //A ecrire dans un fichier
+        /*std::cout << freqLet->size() << ", nbCar = " << nbIte << std::endl; //Affiche la taille de la map et le nombre de caractère analysé pris en compte
+
+        for (std::map<std::string, float>::iterator it = freqLet->begin(); it != freqLet->end(); ++it) {    //Parcours de la liste
+            std::cout << it->first << " has value " << it->second << std::endl; //Pour chaque élément on affiche le caractère et sa fréquence
+        }
+        */
     }
     catch (std::out_of_range & e) { //Si le fichier ne peux pas s'ouvrire
         std::cerr << e.what() << std::endl;
     }
     
     delete freqLet;         //Libère la mémoire
+    delete freqDig;
     return EXIT_SUCCESS;    //FIN
 }
 
